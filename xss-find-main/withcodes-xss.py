dfcbaefd8c4363a258a7db7c779e9885320ef79c
ixelizm <felekburak8@gmail.com>
2022-12-22 20:25:31 +0300

    Formatted via Classes

--- xss-find-main/withcodes-xss.py
+++ xss-find-main/withcodes-xss.py
@@ -1,32 +1,30 @@
-
-
 import requests
 
-print(
-    """
- ##   ##   ####    ######   ##   ##    ####    #####   #####    #######   #####            ##  ##    #####    #####  
- ##   ##    ##     # ## #   ##   ##   ##  ##  ##   ##   ## ##    ##   #  ##   ##           ##  ##   ##   ##  ##   ## 
- ##   ##    ##       ##     ##   ##  ##       ##   ##   ##  ##   ## #    #                  ####    #        #       
- ## # ##    ##       ##     #######  ##       ##   ##   ##  ##   ####     #####   ######     ##      #####    #####  
- #######    ##       ##     ##   ##  ##       ##   ##   ##  ##   ## #         ##            ####         ##       ## 
- ### ###    ##       ##     ##   ##   ##  ##  ##   ##   ## ##    ##   #  ##   ##           ##  ##   ##   ##  ##   ## 
- ##   ##   ####     ####    ##   ##    ####    #####   #####    #######   #####            ##  ##    #####    ##### 
 
-    """
-)
+class XSSFinder:
+  def __init__(self, url: str) -> None:
+    self.page = url + "/index.php?q="
+    self.banner = """
+##   ##   ####    ######   ##   ##    ####    #####   #####    #######   #####            ##  ##    #####    #####
+ ##   ##    ##     # ## #   ##   ##   ##  ##  ##   ##   ## ##    ##   #  ##   ##           ##  ##   ##   ##  ##   ##
+ ##   ##    ##       ##     ##   ##  ##       ##   ##   ##  ##   ## #    #                  ####    #        #
+ ## # ##    ##       ##     #######  ##       ##   ##   ##  ##   ####     #####   ######     ##      #####    #####
+ #######    ##       ##     ##   ##  ##       ##   ##   ##  ##   ## #         ##            ####         ##       ##
+ ### ###    ##       ##     ##   ##   ##  ##  ##   ##   ## ##    ##   #  ##   ##           ##  ##   ##   ##  ##   ##
+ ##   ##   ####     ####    ##   ##    ####    #####   #####    #######   #####            ##  ##    #####    #####
+"""
 
-
-hedef_site = input("Hedef site giriniz:")
-site = ("http://" + hedef_site + "/index.php?q=")
-txt = open("payload.txt", "r")
-pay = txt.read()
-txt.close()
-for payloads in pay.splitlines():
-    istek = requests.post(site + payloads)
-
-    if payloads in istek.text:
+    with open("payload.txt", "r") as txt:
+      self.payloads = [i for i in txt.readlines()]
+    print(self.banner)
+    print("Sekans Başlatıldı!...")
+    self._make_request()
+  def _make_request(self):
+    for p in self.payloads:
+      self.response = requests.post(self.page + p)
+      if p in self.response.text:
         print("XSS açığı bulundu")
         print("İşlenen Payload:" + payloads)
         break
-    else:
-        print("XSS bulunamadı.")
+    print("XSS bulunamadı.")
+finder = XSSFinder(input("Site URL:"))
